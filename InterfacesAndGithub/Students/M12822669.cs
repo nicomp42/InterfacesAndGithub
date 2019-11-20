09ab7d8c564cf86c6d163c454f9c6ad398f83e57
Matthew Willison <willi6m7@mail.uc.edu>
2019-11-20 14:36:29 -0500

    Look! Functionality!

--- InterfacesAndGithub/Students/M12822669.cs
+++ InterfacesAndGithub/Students/M12822669.cs
@@ -8,5 +8,41 @@ namespace InterfacesAndGithub
 {
     class M12822669 : Student
     {
+
+        public M12822669(String MNumber) : base(MNumber) { }
+
+        public override long ComputeLargestPrimeFactor(long num)
+        {
+            long answer = 0;
+            for (long y = num / 2; y > 1; y--)
+            {
+                if (num % y == 0 && IsPrime(y) == true)
+                {
+
+                    answer = y;
+                    break;
+                }
+            }
+            return answer;
+        }
+
+        /// <summary>
+        /// Method that checks a given number for primeness. Reused code from earlier assignments.
+        /// </summary>
+        /// <param name="num">The number being tested for primeness.</param>
+        /// <returns>A boolean</returns>
+        public bool IsPrime(long num)
+        {
+            bool primeness = true;
+            for (int x = 2; x < num; x++)
+            {
+                if (num % x == 0)
+                {
+                    primeness = false;
+                    break;
+                }
+            }
+            return primeness;
+        }
     }
 }
