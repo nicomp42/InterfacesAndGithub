9093437067affbe0ab082ffde7bfed49655b11fd
Thaddeus Fairweather <fairwetd@mail.uc.edu>
2019-11-21 11:24:15 -0500

    2 Methods IsPrime and ComputerLargestPrimeFactor M12527870

--- InterfacesAndGithub/Students/M12527870.cs
+++ InterfacesAndGithub/Students/M12527870.cs
@@ -8,5 +8,30 @@ namespace InterfacesAndGithub
 {
     class M12527870 : Student
     {
+        public M12527870(String MNumber) : base(MNumber) { }
+        public override long ComputeLargestPrimeFactor(long num)
+        {
+            long status = 1;
+            for (long i= num / 2; i >= 1; i--)
+            {
+                if (isPrime(i) && num % i == 0)
+                { status = i; break; }
+            }
+            return status;
+        }
+        private bool isPrime(long num)
+        {
+            bool status = true;
+            long limit = (long)Math.Sqrt(num);
+            for (int i = 2; i <= limit; i++)
+            {
+                if (num % i == 0)
+                {
+                    status = false;
+                    break;
+                }
+            }
+            return status;
+        }
     }
 }
